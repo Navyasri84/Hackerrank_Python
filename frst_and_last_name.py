2a1bf40a1f817d54134bc8d973662df410560c9a
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-12-15 21:27:11 +0530

    Add files via upload

---
+++ frst_and_last_name.py
@@ -0,0 +1,7 @@
+'''print the first line contains the first name, and the second line contains the last name.
+   The length of the first and last names are each ≤10.'''
+
+first_name = input()
+last_name = input()
+
+print("Hello",first_name+" "+last_name+"! You just delved into python.")
